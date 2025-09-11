894f18f2f8acb0f4f425501b25250c9b3f671c61
mojjedrengen <morgan@famtorp.net>
2025-09-11 14:20:06 +0200

    channels is up but programs is deadlocked

--- Philosophers.go
+++ Philosophers.go
@@ -23,6 +23,9 @@ func forkgeneric(resive <-chan string, leftmsg chan<- string, rightmsg chan<- st
 	for true {
 
 		var msg = <-resive
+
+		fmt.Printf("fork got msg: %s and isTaken == %t\n", msg, isTaken)
+
 		if msg == "left" {
 			if isTaken == false {
 				isTaken = true
@@ -63,11 +66,13 @@ func philosofGeneric(leftFork chan<- string, rightFork chan<- string, leftForkMs
 				haveRightFork = true
 			}
 
+			fmt.Printf("%d: rightFork == %t, leftFork == %t\n", philosofer, haveLeftFork, haveRightFork)
+
 			if haveLeftFork == true && haveRightFork == true {
 				state = Eating
 				timesEaten++
-				fmt.Println("%d is now %s", philosofer, PhilosoferStateName[state])
-				fmt.Println("%d has eaten %d times", philosofer, timesEaten)
+				fmt.Printf("%d is now %s\n", philosofer, PhilosoferStateName[state])
+				fmt.Printf("%d has eaten %d times\n", philosofer, timesEaten)
 			} else if haveLeftFork == true && haveRightFork == false {
 				leftFork <- "leave"
 			} else if haveLeftFork == false && haveRightFork == true {
@@ -78,7 +83,7 @@ func philosofGeneric(leftFork chan<- string, rightFork chan<- string, leftForkMs
 			leftFork <- "leave"
 			rightFork <- "leave"
 			state = Thinking
-			fmt.Println("%d is now %s", philosofer, PhilosoferStateName[state])
+			fmt.Printf("%d is now %s\n", philosofer, PhilosoferStateName[state])
 		}
 	}
 }
@@ -89,21 +94,33 @@ var fork3 = make(chan string)
 var fork4 = make(chan string)
 var fork5 = make(chan string)
 
-var philosof1 = make(chan string)
-var philosof2 = make(chan string)
-var philosof3 = make(chan string)
-var philosof4 = make(chan string)
-var philosof5 = make(chan string)
+var philosof1L = make(chan string)
+var philosof2L = make(chan string)
+var philosof3L = make(chan string)
+var philosof4L = make(chan string)
+var philosof5L = make(chan string)
+var philosof1R = make(chan string)
+var philosof2R = make(chan string)
+var philosof3R = make(chan string)
+var philosof4R = make(chan string)
+var philosof5R = make(chan string)
 
 func main() {
 	fmt.Println("Hello World")
 
-	go forkgeneric(fork1, philosof1, philosof5)
-	go forkgeneric(fork2, philosof2, philosof1)
-	go forkgeneric(fork3, philosof3, philosof2)
-	go forkgeneric(fork4, philosof4, philosof3)
-	go forkgeneric(fork5, philosof5, philosof4)
+	go forkgeneric(fork1, philosof1L, philosof5R)
+	go forkgeneric(fork2, philosof2L, philosof1R)
+	go forkgeneric(fork3, philosof3L, philosof2R)
+	go forkgeneric(fork4, philosof4L, philosof3R)
+	go forkgeneric(fork5, philosof5L, philosof4R)
 
-	go philosofGeneric()
+	go philosofGeneric(fork2, fork1, philosof1L, philosof1R, 1)
+	go philosofGeneric(fork3, fork2, philosof2L, philosof2R, 2)
+	go philosofGeneric(fork4, fork3, philosof3L, philosof3R, 3)
+	go philosofGeneric(fork5, fork4, philosof4L, philosof4R, 4)
+	go philosofGeneric(fork1, fork5, philosof5L, philosof5L, 5)
+
+	for true {
+	}
 
 }
